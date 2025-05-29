7ac81c40b4b9847382c0f1aa33abdf89b0846cb2
Naammm <tagiangnamttg@gmail.com>
2025-05-30 01:36:43 +0700

    Update

--- backend/src/main/java/com/school/health/controller/AuthController.java
+++ backend/src/main/java/com/school/health/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.school.health.controller;
 
+import com.school.health.dto.request.RegisterRequest;
+import com.school.health.dto.respone.MessageResponse;
 import com.school.health.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -21,8 +21,17 @@ public class AuthController {
     // @Autowired
     // JwtUtils jwtUtils; // Sẽ inject sau khi cấu hình JWT
 
+//    @PostMapping("/register")
+//    public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterRequest registerRequest) {
+//        try {
+//            return ResponseEntity.ok(new MessageResponse("Người dùng [" + registeredUser.getFullName() + "] đã đăng ký thành công!"));
+//        } catch (RuntimeException e) {
+//            return ResponseEntity.badRequest().body(new MessageResponse("Error: " + e.getMessage()));
+//        }
+//    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(String username, String password) {
-
+        return ResponseEntity.ok(new MessageResponse());
     }
 }
---
+++ backend/src/main/java/com/school/health/security/services/UserDetailsImpl.java
@@ -0,0 +1,101 @@
+package com.school.health.security.services;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.school.health.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private Integer id;
+    private String username;
+    @Getter
+    private String email;
+    @Getter
+    private String fullName;
+
+    @JsonIgnore
+    private String password;
+
+    private boolean isActive;;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Integer id, String username, String email, String fullName, String password, boolean isActive,Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+        this.password = password;
+        this.isActive = isActive;
+        this.authorities = authorities;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new UserDetailsImpl(
+                user.getUserId(),
+                user.getPhone(),
+                user.getEmail(),
+                user.getFullName(),
+                user.getPasswordHash(),
+                user.isActive(),
+                authorities);
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+         return isActive;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
+    }
+}
