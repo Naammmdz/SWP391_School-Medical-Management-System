530c9f6e42dd653493a999008652af2597031ece
nhatmac21 <nhatmac21@gmail.com>
2025-06-09 07:40:15 +0700

    Ignore backend folder

--- backend/src/main/java/com/school/health/enums/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.school.health.enums;
-
-public enum UserRole {
-    PARENT,
-    NURSE,
-    PRINCIPAL,
-    ADMIN
-
-}
--- backend/src/main/java/com/school/health/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.school.health.exception;
-
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
-        super(message);
-    }
-}
