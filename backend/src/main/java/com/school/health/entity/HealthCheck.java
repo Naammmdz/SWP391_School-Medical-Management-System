da6816bac1a5f85ec5f17779ad8466935f7b8409
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2025-06-09 23:33:16 +0700

    them entity HealthCheck va HealthCheckCampaign

--- backend/src/main/java/com/school/health/entity/HealthCheck.java
+++ backend/src/main/java/com/school/health/entity/HealthCheck.java
@@ -1,4 +1,42 @@
 package com.school.health.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HealthCheck {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CheckId")
+    private int checkId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "StudentId")
+    private Student student;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CampaignId")
+    private HealthCheckCampaign campaign;
+    @Column(name = "Date")
+    private LocalDate date;
+    @Column(name = "Height")
+    private double height;
+    @Column(name = "Weight")
+    private double weight;
+    @Column(name = "Eyesight")
+    private String eyesight;
+    @Column(name = "Hearing")
+    private String hearing;
+    @Column(name = "ParentConfirmation")
+    private boolean parentConfirmation;
+    @Column(name = "Notes")
+    private String notes;
+    @Column(name = "CreatedAt")
+    private LocalDateTime createdAt;
+
 }
--- backend/src/main/java/com/school/health/entity/HealthCheckCampaign.java
+++ backend/src/main/java/com/school/health/entity/HealthCheckCampaign.java
@@ -1,4 +1,38 @@
 package com.school.health.entity;
 
+import com.school.health.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class HealthCheckCampaign {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "CampaignId")
+    private int campaignId;
+    @Column(name = "CampaignName")
+    private String campaignName;
+    @Column(name = "Description")
+    private String description;
+    @Column(name = "ScheduledDate")
+    private LocalDate scheduledDate;
+    @Column(name = "CreatedBy")
+    private int createdBy;
+    @Column(name = "ApprovedBy")
+    private int approvedBy;
+    @Column(name = "ApprovedAt")
+    private LocalDateTime approvedAt;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "Status")
+    private Status status;
+    @Column(name = "CreatedAt")
+    private LocalDateTime createdAt;
+
+
 }
